c5839361c12db8da81f76bc67877f18e01593e2f
Vitaly <126074186+VItaly0117@users.noreply.github.com>
2025-06-22 00:51:20 +0300

    Add files via upload

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,6 @@
+﻿namespace Web_site_analytic_sports.Web_site_analytic_sports.Data
+{
+    public class DbInitializer
+    {
+    }
+}
---
+++ Services/IDataService.cs
@@ -0,0 +1,8 @@
+﻿// Services/IDataService.cs
+namespace Web_site_analytic_sports.Services
+{
+    public interface IDataService
+    {
+        Task SeedWorldCupDataAsync(int year);
+    }
+}
